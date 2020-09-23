468fa4ec9299cbc7a8d9df454e8496e056498105
Sacha Vogel <Sacha_Vogel@sluz.ch>
2020-09-23 13:11:45 +0200

    Calculate prices of screws

--- Upload/block03task04.js
+++ Upload/block03task04.js
@@ -1,6 +1,15 @@
-document.querySelector(".reset").addEventListener("click", resetFormular());
+document.getElementById("calculatePriceScrews").onsubmit = function () {
+    let screw = document.getElementById("screw").value;
+    let screwnut = document.getElementById("screwnut").value;
+    let grommet = document.getElementById("grommet").value;
+    
+    let price = 0;
+    price += screw * 20;
+    price += screwnut * 10;
+    price += grommet * 5;
 
-function resetFormular() {
-    let inputs = document.querySelector("input");
-    inputs.innerHTML = "";
-}
+    price = price/100;
+    document.getElementById("price").innerText = "Total: " + price + " CHF";
+
+    return false;
+};
